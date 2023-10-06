e802221ac73d70d4be467feaf1d82ab60609958e
FedeCoyra <83997459+FedeCoyra@users.noreply.github.com>
2023-10-06 16:47:55 -0300

    Add files via upload

---
+++ Trabajo Practico 2/Tp2-Ejercicio9/src/ar/edu/unlu/poo/ejercicio9/Cubo.java
@@ -0,0 +1,21 @@
+package ar.edu.unlu.poo.ejercicio9;
+
+
+public class Cubo extends FiguraTresD {
+    private double arista;
+
+    public Cubo(double arista) {
+        this.arista = arista;
+    }
+
+    @Override
+    public double getArea() {
+        return 6 * arista * arista;
+    }
+
+    @Override
+    public double getVolumen() {
+        return Math.pow(arista, 3);
+    }
+}
+
---
+++ Trabajo Practico 2/Tp2-Ejercicio9/src/ar/edu/unlu/poo/ejercicio9/FiguraTresD.java
@@ -0,0 +1,7 @@
+package ar.edu.unlu.poo.ejercicio9;
+
+public class FiguraTresD extends FiguraDosD {
+    public double getVolumen() {
+        return 0;
+    }
+}
